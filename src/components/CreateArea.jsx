27228561e29e3d5bf154dc999a1bdbe633105e03
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-02 18:47:13 -0400

    Created the component create Area to create new notes

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+function CreateArea() {
+  const [note, setNote] = useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
+  return (
+    <div>
+      <form>
+        <input name="title" value={note.title} onChange={handleChange} placeholder="Title" />
+        <textarea name="content" value={note.content} onChange={handleChange} placeholder="Take a note..." rows="3" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
