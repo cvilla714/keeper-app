a14e56d398595ff9880b03b4613ce7f21132c3a5
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-02 19:58:51 -0400

    added the delted  function as props

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
 function Note(props) {
+  function handleClick() {
+    props.onDelete();
+  }
+
   return (
     <div className="note">
       {/* <h1>Title</h1> */}
       <h1>{props.title}</h1>
       {/* <p>content</p> */}
       <p>{props.content}</p>
+      <button onClick={handleClick}>DELETE</button>
     </div>
   );
 }
