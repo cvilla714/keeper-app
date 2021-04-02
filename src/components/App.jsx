99030462609303d218a4d3906f6f0418629afcff
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-02 19:52:48 -0400

    added hooks and state to handle the state and adding the title and context

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
@@ -6,14 +6,26 @@ import notes from "../notes.js";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notas, setNotas] = useState([]);
+
+  function addNote(note) {
+    // console.log(note);
+    setNotas((prevNotas) => {
+      return [...prevNotas, note];
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
+      <CreateArea onAdd={addNote} />
+      {notas.map((notasItem) => {
+        return <Note title={notasItem.title} content={notasItem.content} />;
+      })}
+
       {notes.map((note) => (
         <Note key={note.id} title={note.title} content={note.content} />
       ))}
-
       <Footer />
     </div>
   );
