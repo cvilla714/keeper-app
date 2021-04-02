86be7c70b9c820e1987b81e10168b05e142d4b50
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-02 18:49:36 -0400

    created the onClick event and added the function submitNote

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -17,12 +17,16 @@ function CreateArea() {
     });
   }
 
+  function submitNote(event) {
+    event.preventDefault();
+  }
+
   return (
     <div>
       <form>
         <input name="title" value={note.title} onChange={handleChange} placeholder="Title" />
         <textarea name="content" value={note.content} onChange={handleChange} placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <button onClick={submitNote}>Add</button>
       </form>
     </div>
   );
