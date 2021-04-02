f182a8c2c72cc75dffe3e96f6a0fb2975877ad06
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-02 19:51:39 -0400

    deleted the exra bracket

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -4,7 +4,7 @@ function Note(props) {
   return (
     <div className="note">
       {/* <h1>Title</h1> */}
-      <h1>{props.title}></h1>
+      <h1>{props.title}</h1>
       {/* <p>content</p> */}
       <p>{props.content}</p>
     </div>
