dfc6a03a79f3604560fe88e76b86efefdd5f0127
Cosmel Villalobos <cosmelvg@gmail.com>
2021-04-02 18:46:52 -0400

    imported the CreateArea file and render it in the Dom

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,13 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes.js";
+import CreateArea from "./CreateArea";
 
 function App() {
   return (
     <div>
       <Header />
+      <CreateArea />
       {notes.map((note) => (
         <Note key={note.id} title={note.title} content={note.content} />
       ))}
